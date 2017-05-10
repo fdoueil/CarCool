db8a5ca42e612ef65e0e3a9c233a9af25ef9020e
fdoueil <frederic.doueil@berger-levrault.com>
2017-05-10 16:46:46 +0200

    Classes

---
+++ src/carcool/com/interfaces/IUserRegisterValidator.java
@@ -0,0 +1,9 @@
+package carcool.com.interfaces;
+
+public interface IUserRegisterValidator {
+	
+	String validateEmail();
+	String validatePwd();
+	String validateName();
+	
+}
--- src/carcool/com/model/Trajet.java
+++ src/carcool/com/model/Trajet.java
@@ -1,5 +1,33 @@
 package carcool.com.model;
 
+import java.util.List;
+
 public class Trajet {
 
+	private int idConducteur;
+	private String depuisAdresse;
+	private String versAdresse;
+	private List<String> etapes;
+	private int sieges;
+	
+	/**
+     * Constructeur.
+     */
+	public Trajet(int idConducteur, String depuisAdresse, String versAdresse, int sieges) {
+		this(idConducteur, depuisAdresse, versAdresse, null, sieges);
+	}
+	
+	public Trajet(int idConducteur, String depuisAdresse, String versAdresse, List<String> etapes, int sieges) {
+		this.idConducteur=idConducteur;
+		this.depuisAdresse= depuisAdresse;
+		this.etapes = etapes;
+		this.sieges = sieges;
+	}
+
+	@Override
+	public String toString() {
+		return "Trajet [idConducteur=" + idConducteur + ", depuisAdresse=" + depuisAdresse + ", versAdresse="
+				+ versAdresse + ", etapes=" + etapes + ", sieges=" + sieges + "]";
+	}
+	
 }
